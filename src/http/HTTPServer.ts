e67d34f74a7152e3cfe0b454aedaf4fe36d317b7
NoSharp <33969918+NoSharp@users.noreply.github.com>
2020-11-03 10:48:52 +0000

    Add HTTPServer class

---
+++ src/http/HTTPServer.ts
@@ -0,0 +1,36 @@
+import {HttpRequest, HttpResponse, SSLApp, TemplatedApp} from "uWebSockets.js";
+
+export default class HTTPServer{
+
+    app : TemplatedApp;
+    port : number;
+
+    /**
+     * Initializes the object.
+     * @param port
+     */
+    constructor(port: number) {
+        this.port = port;
+        this.app = SSLApp({});
+    }
+
+    /**
+     * Used to add a route to our websserver.
+     * @param endpoint
+     * @param callback
+     */
+    addRoute(endpoint: string, callback: (res: HttpResponse, req:HttpRequest) => void){
+        this.app.get(endpoint, callback);
+    }
+
+
+    /**
+     * Starts the webserver on the port
+     * given in the constructor.
+     */
+    listen(callback: ()=>void){
+        this.app.listen(this.port,()=>{
+            callback();
+        });
+    }
+}
